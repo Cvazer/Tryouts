2abc59e40ff761fe06a77704b0d434449d4181d9
Cvazer <zhirnitoni@gmail.com>
2018-04-09 11:54:35 +0300

    Update EmployeeController.java

--- Playgendary/playgendary-tryout-boot/src/main/java/com/github/cvazer/tryout/playgendary/controllers/EmployeeController.java
+++ Playgendary/playgendary-tryout-boot/src/main/java/com/github/cvazer/tryout/playgendary/controllers/EmployeeController.java
@@ -15,6 +15,7 @@ public class EmployeeController {
 
     private EmployeeService service;
 
+    @Autowired
     public EmployeeController(EmployeeService service) {
         this.service = service;
     }
